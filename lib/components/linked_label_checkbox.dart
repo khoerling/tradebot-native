951ef968f3ed96a004c43b3a5e4e9f365378b780
mr. keith <keith@hoerling.com>
2020-04-01 23:45:46 -0700

    must be StatefulWidget so it renders on prop change
    - this is odd

--- lib/components/linked_label_checkbox.dart
+++ lib/components/linked_label_checkbox.dart
@@ -1,44 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 
-class LinkedLabelCheckbox extends StatelessWidget {
+class LinkedLabelCheckbox extends StatefulWidget {
   const LinkedLabelCheckbox({
     this.label,
+    this.subLabel = '',
     this.padding,
     this.value,
     this.onChanged,
   });
 
   final String label;
+  final String subLabel;
   final EdgeInsets padding;
   final bool value;
   final Function onChanged;
 
   @override
+  _LinkedLabelCheckbox createState() => _LinkedLabelCheckbox();
+}
+
+class _LinkedLabelCheckbox extends State<LinkedLabelCheckbox> {
+  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: padding,
+      padding: widget.padding,
       child: Row(
         children: <Widget>[
-          Expanded(
-            child: RichText(
-              text: TextSpan(
-                text: label,
-                style: TextStyle(
-                  color: Colors.blueAccent,
-                  decoration: TextDecoration.underline,
-                ),
-                recognizer: TapGestureRecognizer()
-                  ..onTap = () {
-                    print('Label has been tapped.');
-                  },
+          ConstrainedBox(
+            constraints: new BoxConstraints(
+              minHeight: 40.0,
+              maxHeight: 80.0,
+            ),
+            child: Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 20,
+                    child: Expanded(
+                      child: RichText(
+                        text: TextSpan(
+                          text: widget.label,
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              widget.onChanged(!(widget.value ?? false));
+                            },
+                        ),
+                      ),
+                    ),
+                  ),
+                  widget.subLabel.isNotEmpty
+                      ? Expanded(
+                          child: RichText(
+                            text: TextSpan(
+                              text: widget.subLabel,
+                              style: TextStyle(
+                                  fontSize: 12, color: Colors.white54),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  widget.onChanged(!(widget.value ?? false));
+                                },
+                            ),
+                          ),
+                        )
+                      : Container(),
+                ],
               ),
             ),
           ),
-          Checkbox(
-            value: value,
+          Switch(
+            value: widget.value,
             onChanged: (bool newValue) {
-              onChanged(newValue);
+              widget.onChanged(newValue);
             },
           ),
         ],
