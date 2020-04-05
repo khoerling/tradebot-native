1adef2c6525a9d3278c4c1ab2c604bc5b537bdaa
mr. keith <keith@hoerling.com>
2020-04-05 00:42:30 -0700

    wip: loading alerts from firestore :tada:
    - TODO really delete
    - TODO better gui for alert
    - TODO alert details
    - TODO alert update?

--- lib/models/alert.dart
+++ lib/models/alert.dart
@@ -1,4 +1,5 @@
 import 'package:enum_to_string/enum_to_string.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 enum AlertGuppy { green, red, grey }
 enum AlertName { price, divergence, guppy }
@@ -24,6 +25,22 @@ class Alert {
       this.market,
       this.timeframe,
       this.params});
+
+  Alert.fromSnapshot(DocumentSnapshot json)
+      : id = json['id'],
+        name = json['name'],
+        exchange = json['exchange'],
+        market = json['market'],
+        timeframe = json['timeframe'],
+        params = json['params'];
+
+  Alert.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = EnumToString.fromString(AlertName.values, json['name']),
+        exchange = json['exchange'],
+        market = json['market'],
+        timeframe = json['timeframe'],
+        params = json['params'];
 
   toJson() {
     var ps = params.map((k, v) =>
--- lib/pages/alert_list.dart
+++ lib/pages/alert_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tradebot_native/models/alert.dart';
 
 class AlertList extends StatefulWidget {
   const AlertList({
@@ -10,25 +12,39 @@ class AlertList extends StatefulWidget {
 
 // class _CreateAlert extends State<AlertCreate> {
 class _AlertList extends State<AlertList> {
-  List<int> shades = <int>[50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+  final db = Firestore.instance;
+  List<Alert> alerts = [];
 
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => _fetchAlerts());
+  }
+
+  _fetchAlerts() async {
+    // push alert to firebase
+    CollectionReference ref = await db.collection("alerts");
+    ref.getDocuments().then((QuerySnapshot snapshot) {
+      var _alerts = snapshot.documents
+          .where((alert) => alert.data['name'] != null)
+          .map((alert) => Alert.fromJson(alert.data));
+      print('Loaded: ' + _alerts.length.toString());
+      alerts.addAll(_alerts);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
       child: ListView.builder(
-        itemCount: shades.length,
+        itemCount: alerts.length,
         itemBuilder: (BuildContext context, int index) {
-          final item = shades[index];
+          final item = alerts[index];
           return Dismissible(
               key: Key(item.toString()),
               onDismissed: (direction) {
                 setState(() {
-                  shades.removeAt(index);
+                  alerts.removeAt(index);
                 });
               },
               background: Container(color: Colors.red),
@@ -41,7 +57,7 @@ class _AlertList extends State<AlertList> {
                       Navigator.pushNamed(context, "/alert");
                     },
                     child: Center(
-                      child: Text('Alert ${item}'),
+                      child: Text('Alert ${item.name}'),
                     ),
                   ),
                 ),
