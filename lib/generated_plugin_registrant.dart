c0f0135068942853e94ea5746a41db9cde344e46
mr. keith <keith@hoerling.com>
2020-04-15 00:20:44 -0700

    upgrades to the latest flutter

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -8,6 +8,7 @@ import 'dart:ui';
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:cloud_functions_web/cloud_functions_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -16,5 +17,6 @@ void registerPlugins(PluginRegistry registry) {
   FirestoreWeb.registerWith(registry.registrarFor(FirestoreWeb));
   CloudFunctionsWeb.registerWith(registry.registrarFor(CloudFunctionsWeb));
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
+  SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   registry.registerMessageHandler();
 }
