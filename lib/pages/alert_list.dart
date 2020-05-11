52d6222680b29dd6d7f860f1fbd67a5fae85a115
mr. keith <keith@hoerling.com>
2020-05-10 23:58:36 -0700

    refactor: settle on ListView, after days, ugh...
    - use the separate buildItem fn
    - TODO improve item visually
    - TODO improve and make detail screen useful

--- lib/pages/alert_list.dart
+++ lib/pages/alert_list.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -20,74 +21,71 @@ class AlertList extends StatefulWidget {
 class _AlertList extends State<AlertList> {
   @override
   Widget build(BuildContext context) {
-    // final user = Provider.of<User>(context);
     return Consumer<User>(builder: (context, user, child) {
       if (user.alerts.isEmpty) return Container(); // guard
       // sort alerts alerted desc, created desc
       user.alerts.sort((a, b) {
         if (a.isAlerted && b.isAlerted)
           return a.alerted.last.compareTo(b.alerted.last);
-        if (a.isAlerted && !b.isAlerted) return -1;
-        if (b.isAlerted && !a.isAlerted) return 1;
         return a.created.compareTo(b.created);
       });
-
       return SizedBox.expand(
           child: ListView.separated(
               itemCount: user.alerts?.length ?? 0,
               separatorBuilder: (context, index) => Divider(),
               itemBuilder: (BuildContext context, int index) {
-                final alert = user.alerts[index],
-                    base = alert.market['base'].toLowerCase();
-                return Dismissible(
-                    key: Key(alert.id),
-                    onDismissed: (direction) {
-                      HapticFeedback.selectionClick();
-                      user.alerts =
-                          user.alerts.where((a) => a.id != alert.id).toList();
-                      user.save();
-                      if (user.alerts.length < 1)
-                        EventEmitter.publish('selectPage', 0);
-                    },
-                    secondaryBackground: Container(
-                        alignment: Alignment.centerRight,
-                        child:
-                            Icon(Icons.delete, color: Colors.white, size: 35),
-                        color: Colors.red),
-                    background: Container(
-                        alignment: Alignment.centerLeft,
-                        child:
-                            Icon(Icons.delete, color: Colors.white, size: 35),
-                        color: Colors.red),
-                    child: ListTile(
-                        onTap: () {
-                          user.resetAlert(alert);
-                          setState(() {}); // refresh ui
-                          Navigator.pushNamed(context, '/alert',
-                              arguments: alert);
-                        },
-                        isThreeLine: true,
-                        leading:
-                            Hero(tag: alert.id, child: CryptoIcon(name: base)),
-                        title: Text(
-                            "${alert.market['quote']} ➤ ${alert.exchange.toUpperCase()}"),
-                        subtitle: Text(
-                          subtitleFor(alert.name, alert.params) +
-                              ', ' +
-                              alert.timeframe.toString() +
-                              "\n" +
-                              (alert?.alerted?.isNotEmpty
-                                  ? "Last Alerted ${formatTime(alert.alerted.last.millisecondsSinceEpoch)}"
-                                  : "Created ${formatTime(alert.created.millisecondsSinceEpoch)}"),
-                          style: TextStyle(
-                              color: alert.isAlerted
-                                  ? Colors.white
-                                  : Colors.white.withOpacity(.7)),
-                        ),
-                        trailing: Icon(Icons.keyboard_arrow_right,
-                            color: Colors.white.withOpacity(.1), size: 30.0)));
+                return _buildItem(user, user.alerts[index], index);
               }));
     });
+  }
+
+  Widget _buildItem(User user, Alert alert, int index) {
+    final base = alert.market['base'].toLowerCase();
+    return Dismissible(
+        key: Key(alert.id),
+        onDismissed: (direction) {
+          HapticFeedback.selectionClick();
+          user.alerts = user.alerts.where((a) => a.id != alert.id).toList();
+          user.save();
+          if (user.alerts.length < 1) EventEmitter.publish('selectPage', 0);
+        },
+        secondaryBackground: Container(
+            alignment: Alignment.centerRight,
+            child: Icon(Icons.delete, color: Colors.white, size: 35),
+            color: Colors.red),
+        background: Container(
+            alignment: Alignment.centerLeft,
+            child: Icon(Icons.delete, color: Colors.white, size: 35),
+            color: Colors.red),
+        child: ListTile(
+            onTap: () {
+              Navigator.pushNamed(context, '/alert', arguments: alert);
+              Timer(Duration(milliseconds: 10), () {
+                user.resetAlert(alert);
+                setState(() {}); // refresh ui
+              });
+            },
+            isThreeLine: true,
+            leading: Hero(tag: alert.id, child: CryptoIcon(name: base)),
+            title: Text(
+                "${alert.market['quote']} ➤ ${alert.exchange.toUpperCase()}"),
+            subtitle: Text(
+              subtitleFor(alert.name, alert.params) +
+                  ', ' +
+                  index.toString() +
+                  alert.timeframe.toString() +
+                  "\n" +
+                  (alert?.alerted?.isNotEmpty ?? false
+                      ? "Last Alerted ${formatTime(alert.alerted.last.millisecondsSinceEpoch)}"
+                      : "Created ${formatTime(alert.created.millisecondsSinceEpoch)}"),
+              style: TextStyle(
+                  color: alert.isAlerted
+                      ? Colors.white
+                      : Colors.white.withOpacity(.5)),
+            ),
+            trailing: Icon(Icons.keyboard_arrow_right,
+                color: Colors.white.withOpacity(alert.isAlerted ? .8 : .1),
+                size: 30.0)));
   }
 
   subtitleFor(AlertName name, params) {
