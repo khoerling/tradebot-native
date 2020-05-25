7dd4ccb6342694b9f90fda904afb444da7392a77
mr. keith <keith@hoerling.com>
2020-05-24 23:03:41 -0700

    adds Fira font

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
+          fontFamily: 'fira',
           brightness: Brightness.dark,
           backgroundColor: beppuTerminalBlue,
           scaffoldBackgroundColor: withOpacity,
